2be6425b2aa5a8a27dfb8976f828e7149b3c517e
Rakesh sahu <rakesh.sahu888444@gmail.com>
2023-08-12 16:03:13 +0530

    added new checkout function

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+let shopItemData = [
+  {
+    id: "productno1",
+    name: "Shirt n jeans",
+    price: "200",
+    desc: "New arrivals of Casual Shirt 👕 and jeans👖 Combo.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "productno2",
+    name: "Short n t-shirt",
+    price: "100",
+    desc: "New arrivals of Premium short 🩳 n t-shirts 🎽.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "productno3",
+    name: "Denim n T-shirt",
+    price: "250",
+    desc: "New arrivals of Premium denims 👖 n t-shirts 👕.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "productno4",
+    name: "OFF-W short",
+    price: "120",
+    desc: "New arrivals in shorts 🩳.",
+    img: "images/img-4.jpg",
+  },
+  {
+    id: "productno5",
+    name: "block p shirt",
+    price: "120",
+    desc: "New arrivals in block prints.",
+    img: "images/img-5.jpg",
+  },
+  {
+    id: "productno6",
+    name: "flannels shirt",
+    price: "120",
+    desc: "New arrivals in flannels.",
+    img: "images/img-6.jpg",
+  },
+  {
+    id: "productno7",
+    name: "Jack & Jones",
+    price: "300",
+    desc: "New Arrivals JNJ.",
+    img: "images/img-7.jpg",
+  },
+  {
+    id: "productno8",
+    name: "Jack & Jones",
+    price: "180",
+    desc: "New Arrivals JNJ.",
+    img: "images/img-8.jpg",
+  },
+];
